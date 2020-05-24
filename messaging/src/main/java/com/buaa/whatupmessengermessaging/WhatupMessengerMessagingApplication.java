0e7d5f47437928cdce91c7e0c4f8de433f464d3a
lyx <383625226@qq.com>
2020-05-24 18:44:08 +0800

    init messaging module

---
+++ messaging/src/main/java/com/buaa/whatupmessengermessaging/WhatupMessengerMessagingApplication.java
@@ -0,0 +1,13 @@
+package com.buaa.whatupmessengermessaging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatupMessengerMessagingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhatupMessengerMessagingApplication.class, args);
+    }
+
+}
---
+++ messaging/src/test/java/com/buaa/whatupmessengermessaging/WhatupMessengerMessagingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buaa.whatupmessengermessaging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatupMessengerMessagingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
